4e332bd2da0df56c2fd3ba3dfd1933656f84406c
Bridget Galang <galanbri@gmail.com>
2023-02-01 08:30:40 -0800

    added homepage link

--- src/upcoming-events.tsx
+++ src/upcoming-events.tsx
@@ -12,9 +12,9 @@ export function UpcomingEvents(props: UpcomingEvents) {
         <tr>
             <td style={{ width: '400px', padding: '10px' }}>{name}</td>
             <td style={{ width: '100px', padding: '10px' }}>{time}</td>
-            {date.length === 0 ? <>
+        {date.length === 0 ? <>
             <td style={{ width: '50px', padding: '10px' }}>{date}</td>
-            </>: <td style={{ width: '50px', padding: '10px', color: 'red' }}>TBA</td>}
+        </>: <td style={{ width: '50px', padding: '10px', color: 'red' }}>TBA</td>}
         </tr>
     </>
 }
