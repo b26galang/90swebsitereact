372afb58d9ed26b1a3e0a4865b0464983d7ef7ec
Bridget Galang <galanbri@gmail.com>
2023-02-01 08:40:10 -0800

    fixed conditional rendering

--- src/upcoming-events.tsx
+++ src/upcoming-events.tsx
@@ -13,8 +13,9 @@ export function UpcomingEvents(props: UpcomingEvents) {
             <td style={{ width: '400px', padding: '10px' }}>{name}</td>
             <td style={{ width: '100px', padding: '10px' }}>{time}</td>
         {date.length === 0 ? <>
-            <td style={{ width: '50px', padding: '10px' }}>{date}</td>
-        </>: <td style={{ width: '50px', padding: '10px', color: 'red' }}>TBA</td>}
+            <td style={{ width: '50px', padding: '10px', color: 'red' }}>TBA</td>
+        </>: 
+        <td style={{ width: '50px', padding: '10px' }}>{date}</td>}
         </tr>
     </>
 }
